37757ddd4b930b73159f55837a44a3e2ed893cb8
Elsebasdrk17 <48342352+Elsebasdrk17@users.noreply.github.com>
2019-04-12 15:24:13 -0500

    Se subieron los archivos del proyecto
    Contiene:
    Clase Producto
    Formulario del proyecto

---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ticketTienda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Se hacen globales para que el valor se obtenga en todo el formulario
+        /// </summary>
+        double subtotal, total;
+
+        Producto producto;
+
+        private void btnPagar_Click(object sender, EventArgs e)
+        {
+            if (txtSuPago.Text == "")
+            {
+                MessageBox.Show("Obligatorio poner la cantidad pagada", "ERROR");
+            }
+            else
+            {
+                double pago = Convert.ToDouble(txtSuPago.Text);
+                if (pago > total)
+                {
+                    txtCambio.Text = (pago - total).ToString();
+                }
+                else
+                {
+                    txtCambio.Text = "0";
+                }
+                total -= pago;
+                txtSubTotal.Text = subtotal.ToString();
+                if (total < 0)
+                {
+                    total = 0;
+                    txtTotal.Text = total.ToString();
+                }
+                else
+                {
+                    txtTotal.Text = total.ToString();
+                }
+            }
+        }
+
+        private void btnTicket_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            string nombreArchivo;
+            nombreArchivo = saveFileDialog1.FileName;
+            FileStream fs = new FileStream(nombreArchivo, FileMode.Create);
+            StreamWriter escritor = new StreamWriter(fs);
+            escritor.WriteLine("EL OTZO");
+            escritor.WriteLine(DateTime.Now.ToString());
+            for(int i = 0; i < listaProductos.Items.Count; i++)
+            {
+                escritor.WriteLine(listaProductos.Items[i]);
+            }
+            escritor.Write("Importe: $");
+            escritor.WriteLine(txtImporte.Text);
+            escritor.Write("IVA: $");
+            escritor.WriteLine(txtIVA.Text);
+            escritor.Write("Total: $");
+            double totalTicket = subtotal + producto.IVA(subtotal);
+            escritor.WriteLine(totalTicket);
+            escritor.Write("Pagaste: $");
+            escritor.WriteLine(txtSuPago.Text);
+            escritor.Write("Te resta: $");
+            escritor.WriteLine(total);
+            escritor.Write("Tu cambio: $");
+            escritor.WriteLine(txtCambio.Text);
+            escritor.WriteLine("¡Gracias por tu compra!");
+            escritor.Close();
+            fs.Close();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtProducto.Text;
+            int cantidad = Convert.ToInt32(txtCantidad.Text);
+            if (txtCosto.Text == "")
+            {
+                MessageBox.Show("Obligatorio poner precio del producto", "ERROR");
+            }
+            else
+            {
+                double costo = Convert.ToDouble(txtCosto.Text);
+                producto = new Producto(nombre, cantidad, costo);
+                txtImporte.Text = producto.importe().ToString();
+                subtotal += producto.importe();
+                txtSubTotal.Text = subtotal.ToString();
+                txtIVA.Text = producto.IVA(subtotal).ToString();
+                total = subtotal + producto.IVA(subtotal);
+                txtTotal.Text = total.ToString();
+                listaProductos.Items.Add(producto.ToString());
+            }
+        }
+    }
+}
---
+++ Producto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ticketTienda
+{
+    class Producto
+    {
+        private string _nombre;
+        private int _cantidad;
+        private double _costo;
+        private double _total;
+        public Producto(string nombre,int cantidad, double costo)
+        {
+            _nombre = nombre;
+            _cantidad = cantidad;
+            _costo = costo;
+            _total = _costo * _cantidad;
+        }
+        public string nombre
+        {
+            get { return _nombre; }
+        }
+        public int cantidad
+        {
+            get { return _cantidad; }
+        }
+        public double costo
+        {
+            get { return _costo; }
+        }
+        public double total
+        {
+            get { return _total; }
+        }
+        public double IVA(double subTotal)
+        {
+            return subTotal * 0.16;
+        }
+        public double importe()
+        {
+            return _costo * _cantidad;
+        }
+        public override string ToString()
+        {
+            return "Producto: " + _nombre
+                + " Cantidad: " + _cantidad
+                + " Precio Unitario: $" + _costo
+                + " Total: " + _total;
+        }
+    }
+}
